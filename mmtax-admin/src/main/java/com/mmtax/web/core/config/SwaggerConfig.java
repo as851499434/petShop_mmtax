f76fd0f4bcdf3ff5392fe4bd6c5e701d508f5f5f
liangfan <liangfan1104@163.com>
2021-04-05 21:31:35 +0800

    delete noting

--- mmtax-admin/src/main/java/com/mmtax/web/core/config/SwaggerConfig.java
+++ mmtax-admin/src/main/java/com/mmtax/web/core/config/SwaggerConfig.java
@@ -38,21 +38,5 @@ public class SwaggerConfig
                 .build();
     }
 
-//    /**
-//     * 添加摘要信息
-//     */
-//    private ApiInfo apiInfo()
-//    {
-//        // 用ApiInfoBuilder进行定制
-//        return new ApiInfoBuilder()
-//                // 设置标题
-//                .title("标题：若依管理系统_接口文档")
-//                // 描述
-//                .description("描述：用于管理集团旗下公司的人员信息,具体包括XXX,XXX模块...")
-//                // 作者信息
-//                .contact(new Contact(Global.getName(), null, null))
-//                // 版本
-//                .version("版本号:" + Global.getVersion())
-//                .build();
-//    }
+
 }
